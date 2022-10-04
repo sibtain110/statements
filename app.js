4170348d95c1e71d81df35f16ffa786a357593b6
sibtain110 <handsomepro125@gmail.com>
2022-10-04 03:34:55 -0700

    statements

---
+++ app.js
@@ -0,0 +1,138 @@
+// split
+// let text = "hmd"
+// console.log(text)
+// let text1 = text.split()
+// console.log(text1)
+
+// let a = ["ali", 22 ,"gilgit"]
+
+// a.pop()
+// console.log(a)
+
+// a.push("danyore")
+// console.log(a);
+
+// a.shift()
+// console.log(a);
+
+// a.unshift("abbas")
+// console.log(a);
+
+
+
+// let array = ["hello world"];
+// array.split(" ")
+// console.log(array);
+
+
+// ||
+
+// let x = 5
+
+// console.log( x < 10 && x > 3);
+
+// console.log( x < 10 || x > 3);
+
+// console.log( !(x < 10 && x > 3));
+
+// console.log( !(x > 10 ));
+
+
+
+// logical opertors
+
+
+// // &&
+
+// let x = 4
+
+// console.log( x < 10 && x > 3);
+
+
+// console.log( x < 10 && x > 6);
+
+
+
+// // ||
+
+// let y = 5
+
+// console.log( y <  5 || y > 6);
+
+
+// // !
+
+// let z = 100
+
+// console.log(!( z < 99 && z > 101));
+
+// console.log(!(z < 101 && z > 10));
+
+
+// let color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
+// let o = ["th","st","nd","rd"]
+
+// console.log("1st choice is " , color[0]);
+// console.log("2nd choice is " , color[1]);
+// console.log("3rd choice is " , color[2]);
+
+// console.log(1, o[1], "choice is " , color[0]);
+
+// console.log(2, o[2], "choice is " , color[1]);
+
+// console.log(3, o[3], "choice is " , color[2]);
+
+// let choice1 = color[0]
+// console.log("1st choice is",choice1);
+
+// let choice2 = color[1]
+// console.log("2nd choice is",choice2);
+
+// let choice3 = color[2]
+// console.log("3rd choice is",choice3);
+
+
+
+// statements
+
+
+let age = prompt("Enter your age")
+
+if (age <= 12 && age >= 0){
+    alert("you are young")
+}
+else if(age < 0){
+    alert("enter valid age")
+}
+else if(age <=19){
+    alert("you are a teenager")
+}
+else if(age <= 30){
+    alert("you are mature")
+}
+else if(age <= 50){
+    alert("you are man")
+}
+else if(age <= 70){
+    alert("you are getting old")
+}
+else if(age <= 100){
+    alert("you are an old man")
+}
+else{
+    alert("you are near to death")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
